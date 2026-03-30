3e2e891634cb6bcd6bb775cc45c009e158090cd7
rwop23 <rwop299@gmail.com>
2026-03-30 17:33:13 +0300

    Q2

---
+++ Q2.py
@@ -0,0 +1,16 @@
+nums = []
+
+n = int(input("enter the length of the list: "))
+
+
+for i in range(n):
+    num = int(input("enter the number: "))
+    nums.append(num)
+
+largest = nums[0]
+
+for x in nums:
+    if x > largest:
+        largest = x
+
+print("the largest number is : ", largest)
