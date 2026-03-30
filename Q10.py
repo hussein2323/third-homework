11e1b1c43784c29422fb866537297b20b719f798
rwop23 <rwop299@gmail.com>
2026-03-30 18:26:27 +0300

    Q10

---
+++ Q10.py
@@ -0,0 +1,8 @@
+def sum(a, b):
+    result = a + b
+    print("the sum is ", result)
+
+num1 = int(input("enter the first number: "))
+num2 = int(input("enter the second number: "))
+
+sum(num1, num2)
