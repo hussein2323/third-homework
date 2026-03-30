aa75b68416ee89c7dd08e19188c0a138478581f0
rwop23 <rwop299@gmail.com>
2026-03-30 18:28:53 +0300

    Q11

---
+++ Q11.py
@@ -0,0 +1,8 @@
+def rectangle(length, width):
+    area = length * width
+    return area
+
+# Example usage:
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+print("The area of the rectangle is:", rectangle(length, width))
