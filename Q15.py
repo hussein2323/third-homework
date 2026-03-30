fcb9412a0b402db954e86d7f5dfeb5e0d3cea650
rwop23 <rwop299@gmail.com>
2026-03-30 19:08:12 +0300

    Q15

---
+++ Q15.py
@@ -0,0 +1,14 @@
+def check(username, password):
+    if not username or not password:
+        return False
+    if len(password) < 8:
+        return False
+    return True
+
+user2 = input("Enter your username: ")
+password2 = input("Enter your password: ")
+
+if check(user2, password2):
+    print("Credentials are valid")
+else:
+    print("Invalid username or password")
