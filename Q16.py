e260fd52d8431e78329409236b8fe45f80e8b35b
rwop23 <rwop299@gmail.com>
2026-03-30 19:12:13 +0300

    Q15

---
+++ Q16.py
@@ -0,0 +1,18 @@
+import random
+
+def random_number():
+    return random.randint(1, 100)
+
+def guess(target):
+    while True:
+        guess = int(input("Guess the number (1-100): "))
+        if guess < target:
+            print("Too low!")
+        elif guess > target:
+            print("Too high!")
+        else:
+            print("good job ")
+            break
+
+number_to_guess = random_number()
+guess(number_to_guess)
